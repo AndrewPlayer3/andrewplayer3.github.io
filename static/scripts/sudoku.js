6df4915e6c3b7ebf8d325ebe8abaa28f955b09b5
Andrew Player <andrewplayer3@gmail.com>
2020-12-11 05:15:37 -0600

    kind of finished

--- static/scripts/sudoku.js
+++ static/scripts/sudoku.js
@@ -3,46 +3,68 @@
 //  https://www.youtube.com/watch?v=G_UYXzGuqvM
 //  https://sudoku.game/
 
-count = 0
+let empty_grid = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+let solved = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
 
 function possible(board, y, x, n) {
-    for(i = 0; i < 9; i++) {
-        if(board[y][i] === n) {
+    for (var i = 0; i < 9; i++) {
+        if (board[y][i] === n) {
             return false;
         }
-        if(board[i][x] === n) {
+        if (board[i][x] === n) {
             return false;
         }
     }
-    y0 = Math.floor(y/3)*3;
-    x0 = Math.floor(x/3)*3;
-    for(i = 0; i < 3; i++) {
-        for(j = 0; j < 3; j++) {
-            if(board[i+y0][j+x0] === n) {
+    y0 = Math.floor(y / 3) * 3;
+    x0 = Math.floor(x / 3) * 3;
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+            if (board[y0+i][x0+j] === n) {
                 return false;
             }
         }
     }
     return true;
-} 
+}
 
-function solver(board) {
-    console.log(count++)
-    for(y = 0; y < 9; y++) {
-        for(x = 0; x < 9; x++) {
-            if(board[y][x] === 0) {
-                for(n = 1; n < 10; n++) {
-                    if(possible(board, y, x, n)) {
+function solver(board, count) {
+    for (var y = 0; y < 9; y++) {
+        for (var x = 0; x < 9; x++) {
+            if (board[y][x] === 0) {
+                for (var n = 1; n < 10; n++) {
+                    if (possible(board, y, x, n)) {
                         board[y][x] = n;
-                        solver(board);
-                        board[y][x] = 0;
-                    }
+                        count = solver(board, count);
+                        board[y][x] = 0; 
+                    } 
                 }
-                return;
+              return count
             }
         }
     }
-    console.log(board);
+    count = count + 1;
+    return count
 }
 
 function shuffle(array) {
@@ -64,14 +86,108 @@ function shuffle(array) {
     return array;
 }
 
-grid = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9]
-];
+function checkBoard(board) {
+    for(var row = 0; row < 9; row++) {
+        for(var col = 0; col < 9; col++) {
+            if(board[row][col] === 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function fillBoard(board) {
+    for(var i = 0; i < 81; i++) {
+        var row = Math.floor(i / 9);
+        var col = i % 9;
+        if(board[row][col] === 0) {
+            numberList = shuffle(numberList);
+            for(var number of numberList) {
+                var not_in_row = true;
+                for(var c of board[row]) {
+                    if(c === number) {
+                        not_in_row = false;
+                    }
+                }
+                var not_in_col = true;
+                for(var r of board) {
+                    if(r[col] === number) {
+                        not_in_col = false;
+                    }
+                }
+                if(not_in_row) {
+                    if(not_in_col) {
+                        var row_bounds = Math.floor(row/3)*3;
+                        var rows = board.slice(row_bounds, row_bounds+3);
+                        var col_bounds = Math.floor(col/3)*3;
+                        var square = []
+                        for(var i = row_bounds; i < row_bounds+3; i++) {
+                            for(var j = col_bounds; j < col_bounds+3; j++) {
+                                square.push(board[i][j]);
+                            }
+                        }
+                        var not_in_box = true;
+                        for(var v of square) {
+                            if(v === number) {
+                                not_in_box = false;
+                            }
+                        }
+                        if(not_in_box) {
+                            board[row][col] = number;
+                            solved[row][col] = number;
+                            if(checkBoard(board)) {
+                                return true;
+                            } else if(fillBoard(board)) {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            break;
+        }
+    }
+    board[row][col] = 0;
+    solved[row][col] = 0;
+}
+
+function pruneBoard(board) {
+    positions = [];
+    for(var i = 0; i < 81; i++) {
+        row = Math.floor(i / 9);
+        col = i % 9;
+        if(board[row][col] != 0) {
+            positions.push(i);
+        }
+    }
+    positions = shuffle(positions);
+    for(var position of positions.slice(0, 60)) {
+        y = Math.floor(position / 9);
+        x = position % 9;
+        temp = board[y][x];
+        board[y][x] = 0;
+        count = solver(board, 0);
+        if(count != 1) {
+            board[y][x] = temp;
+        }
+        count = 0;
+    }
+}
+
+function checkIfSolved(board) {
+    if(checkBoard(board)) {
+        for(var i = 0; i < 81; i++) {
+            row = Math.floor(i / 9);
+            col = i % 9;
+            if(empty_grid[row][col] != solved[row][col]) {
+                return false;
+            }
+            board[row][col] = temp;
+        }
+        return true;
+    }
+    return false;
+}
--- sudoku.py
+++ sudoku.py
@@ -11,7 +11,21 @@ grid = [
     [0, 6, 0, 0, 0, 0, 2, 8, 0],
     [0, 0, 0, 4, 1, 9, 0, 0, 5],
     [0, 0, 0, 0, 8, 0, 0, 7, 9]
-];
+]
+
+empty_grid = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
+recurses = 0
 
 # Solver and Possible from Computerphile Tutorial
 
@@ -34,13 +48,13 @@ board = []
 
 def solver(grid):
     global count
-    global board
+    global recurses
+    recurses += 1
     for y in range(9):
         for x in range(9):
             if grid[y][x] == 0:
                 for n in range(1,10):
                     if possible(grid, y, x, n):
-                        print(y,", ", x)
                         grid[y][x] = n
                         solver(grid)
                         grid[y][x] = 0
@@ -49,6 +63,7 @@ def solver(grid):
 
 def generateBoard():
     global count    
+    global recurses
     length = 81    
     board = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -69,7 +84,9 @@ def generateBoard():
             col = position %  9
             if possible(board, row, col, i):
                 board[row][col] = i
-    solver(board)   
+    solver(board)
+    print(recurses)
+    recurses = 0   
     if count != 1:
         count = 0
         return generateBoard()
@@ -104,4 +121,67 @@ def countNonZero(board):
                 nonzero += 1
     return nonzero
 
-solver(grid)
+
+
+#A function to check if the grid is full
+def checkGrid(grid):
+  for row in range(0,9):
+      for col in range(0,9):
+        if grid[row][col]==0:
+          return False
+
+  #We have a complete grid!  
+  return True 
+
+numberList=[1,2,3,4,5,6,7,8,9]
+#shuffle(numberList)
+
+#A backtracking/recursive function to check all possible combinations of numbers until a solution is found
+def fillGrid(grid):
+  #Find next empty cell
+  for i in range(0,81):
+    row=i//9
+    col=i%9
+    if grid[row][col]==0:
+      shuffle(numberList)      
+      for value in numberList:
+        #Check that this value has not already be used on this row
+        if not(value in grid[row]):
+          #Check that this value has not already be used on this column
+          if not value in (grid[0][col],grid[1][col],grid[2][col],grid[3][col],grid[4][col],grid[5][col],grid[6][col],grid[7][col],grid[8][col]):
+            #Identify which of the 9 squares we are working on
+            square=[]
+            if row<3:
+              if col<3:
+                square=[grid[i][0:3] for i in range(0,3)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(0,3)]
+              else:  
+                square=[grid[i][6:9] for i in range(0,3)]
+            elif row<6:
+              if col<3:
+                square=[grid[i][0:3] for i in range(3,6)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(3,6)]
+              else:  
+                square=[grid[i][6:9] for i in range(3,6)]
+            else:
+              if col<3:
+                square=[grid[i][0:3] for i in range(6,9)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(6,9)]
+              else:  
+                square=[grid[i][6:9] for i in range(6,9)]
+            #Check that this value has not already be used on this 3x3 square
+            if not value in (square[0] + square[1] + square[2]):
+              grid[row][col]=value
+              if checkGrid(grid):
+                return True
+              else:
+                if fillGrid(grid):
+                  return True
+      break
+  grid[row][col]=0     
+
+filled = fillGrid(empty_grid)
+print(np.matrix(empty_grid))
